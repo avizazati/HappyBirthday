6aef846a4faf4926d42e9196ccc9494a13f6cba3
Avi Sapir <Avi@cordio-med.com>
2021-08-07 18:36:52 +0300

    Added birthday screen layout

---
+++ HappyBirthday/ViewControllers/BirthdayScreenVC.swift
@@ -0,0 +1,33 @@
+//
+//  BirthdayScreenVC.swift
+//  HappyBirthday
+//
+//  Created by Avi Sapir on 07/08/2021.
+//
+
+import UIKit
+
+class BirthdayScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
